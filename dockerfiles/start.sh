c5ff7a547da3489dd11b037e77401e226ef17989
choisungwook <kgg1959@navaer.com>
2020-11-24 00:50:04 +0900

    Fix jmeter start script

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -8,6 +8,7 @@ docker-compose up -d
 docker-compose scale master=1 slave=$COUNT
 
 jmx_filename=$2
+run_jmx_filename="run.jmx"
 WEBAPP_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep webapp | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
 SLAVE_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep slave | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
 
@@ -18,6 +19,6 @@ echo "=============SLAVE_IP LIST"
 echo $SLAVE_IP
 echo "====================="
 
-sed  s/TARGET_IP/$WEBAPP_IP/g test.jmx > test2.jmx
-docker cp $jmx_filename master:/
-docker exec -it master /bin/bash -c "jmeter -n -t $jmx_filename -R$SLAVE_IP"
+sed  s/TARGET_IP/$WEBAPP_IP/g test.jmx > $run_jmx_filename
+docker cp $run_jmx_filename master:/
+docker exec -it master /bin/bash -c "jmeter -n -t run.jmx -R$SLAVE_IP"
