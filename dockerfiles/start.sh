51b8c4e86c462dd209a6d2c609bce82b4fc678a6
choisungwook <kgg1959@navaer.com>
2020-11-27 01:05:05 +0900

    Change 리팩토링

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -6,21 +6,14 @@ COUNT=$1
 docker-compose up -d
 docker-compose scale client=1 server=$COUNT
 
-jmx_filename=$2
 run_jmx_filename=$2
+docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
 WEBAPP_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep webapp | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
 SERVER_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep server | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
 
-echo "=============== WEBAPP IP============="
-echo $WEBAPP_IP
-
-echo "=============SERVER LIST"
-echo $SERVER_IP
-
-docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
 
 sed  s/TARGET_IP/$WEBAPP_IP/g $run_jmx_filename > run.jmx
 docker cp run.jmx client:/
 
 # run client
-docker exec -it client /bin/bash -c "jmeter -n -t run.jmx -R$SERVER_IP"
+docker exec -it client /bin/bash -c "jmeter -n -t /run.jmx -R$SERVER_IP"
